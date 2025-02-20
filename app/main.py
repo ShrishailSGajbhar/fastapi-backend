e0e331d28ad456118116f13aaee21c4e880d744f
ShrishailSGajbhar <shrishailgajbhar5@gmail.com>
2025-02-20 17:04:50 +0530

    Added the Pydantic classes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,50 @@
 from fastapi import FastAPI, Query
-from typing import Union
+from typing import Optional, Union
+from datetime import date
+
+from pydantic import BaseModel
+#Use Optional to make a parameter optional and if not provided it will be None
+#Use Query to add validation to the parameters
+
 
 app = FastAPI()
 
-@app.get("/hotels/")
+class SHotel(BaseModel):
+    name: str
+    location: str
+    stars: int
+    has_pool: bool
+    price: float
+
+@app.get("/hotels/", response_model=list[SHotel])
 async def get_hotels(location: str, 
-                     datefrom: str, 
-                     dateto: str, 
-                     has_pool: Union[bool,None], 
-                     stars: Union[int, None]=Query(None, le=5, ge=1)):
+                     datefrom: Union[date,str], 
+                     dateto: Union[date,str], 
+                     has_pool: Optional[bool], 
+                     stars: Optional[int]=Query(None, le=5, ge=1)) -> list[SHotel]:
+    
+    return [{"name": "Hotel A",
+            "location": location,
+            "stars": stars,
+            "has_pool": has_pool,
+            "price": 100.0}]
+
+async def get_hotels(location: str, 
+                     datefrom: date, 
+                     dateto: date, 
+                     has_pool: Optional[bool], 
+                     stars: Optional[int]=Query(None, le=5, ge=1)) :
     
     return {"hotels": "all"}
+
+class SBookings(BaseModel):
+    room_id: int
+    datefrom: date
+    dateto: date
+    guests: int
+    has_children: bool
+    price: float
+
+@app.post("/bookings/")
+async def create_booking(booking: SBookings):
+    return {"booking": booking}
